d595182b0b24d0037a12ec044e72eadcaad39c13
charles-ram <ram.charles429@gmail.com>
2023-03-11 18:57:46 -0600

    added views and rendered page

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3001);
 
