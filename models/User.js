5306ee486b0e357edba2510daefd41abeda69e12
charles-ram <ram.charles429@gmail.com>
2023-03-14 00:09:37 -0500

    added character card partial

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config.connection');
+const sequelize = require('../config/connection');
 
 class User extends Model {
     checkPassword(loginPw) {
@@ -35,5 +35,20 @@ User.init(
             len: [6],
           },
         },
+      },
+      {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
       }
-)
+);
+
+module.exports = User;
